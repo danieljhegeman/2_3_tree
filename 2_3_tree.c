4fbd096c35b7163cffe5a5cabf3b9ea4001ae2e3
Daniel J Hegeman <danieljhegeman@gmail.com>
2018-05-03 19:44:39 -0700

    Populate initial root node for up to 3 items

--- 2_3_tree.c
+++ 2_3_tree.c
@@ -3,6 +3,9 @@
 #include <string.h>
 
 struct files * setInputAndOutput(int argc, char *args[]);
+struct node * newNode(int val);
+void valOneSwap(struct node * treeNode, struct node * newNode);
+void insert(struct node * root, int val);
 
 struct node {
   int isLeaf;
@@ -18,21 +21,86 @@ struct files {
   FILE *output;
 };
 
+static int count = 0;
+
 int main(int argc, char *argv[])
 {
-  struct node *head = malloc(sizeof(head));
-  head->isLeaf = 1;
+  struct node *root = newNode(0);
+  struct node *node = newNode(0);
   struct files *io = setInputAndOutput(argc, argv);
-  int count;
-  char *input;
+  char *input = NULL;
   size_t max = 10;
-  for (count = 0; getline(&input, &max, io->input) != -1; ++count) {
-    if (io->output == stdout) {
-      printf("Entered %d\n", atoi(input));
+  while (getline(&input, &max, io->input) != -1) {
+    node = newNode(atoi(input));
+    fprintf(stderr, "%d\n", node->valOne);
+    if (count < 3) {
+      if (root->left) {
+        if (node->valOne <= root->valOne) {
+          if (node->valOne == root->valOne) {
+            continue;
+          }
+          root->valOne = node->valOne;
+          valOneSwap(root->left, node);
+        }
+        if (root->middle) {
+          if (node->valOne <= root->valTwo) {
+            if (node->valOne == root->valTwo) {
+              continue;
+            }
+            root->valTwo = node->valOne;
+            valOneSwap(root->middle, node); 
+          }
+          root->right = node;
+        } else {
+          root->valTwo = node->valOne;
+          root->middle = node;
+        }
+      } else {
+        root->valOne = node->valOne;
+        root->left = node;
+      }
     } else {
-      fprintf(io->output, "Entered %d\n", atoi(input));
+      printf("Count is >= 3\n");
+      printf("Root valOne: %d\n", root->valOne);
+      printf("Root left->valOne: %d\n", root->left->valOne);
+      printf("Root valTwo: %d\n", root->valTwo);
+      printf("Root middle->valOne: %d\n", root->middle->valOne);
+      printf("Root right->valOne: %d\n", root->right->valOne);
+//    if (io->output == stdout) {
+//      printf("Entered %d\n", atoi(input));
+//    } else {
+//      fprintf(io->output, "Entered %d\n", atoi(input));
+    }
+    ++count;
+  }
+}
+
+void valOneSwap(struct node * treeNode, struct node * newNode) {
+  int tempVal = treeNode->valOne;
+  treeNode->valOne = newNode->valOne;
+  newNode->valOne = tempVal;
+}
+
+void insert(struct node * root, int val) {
+  if (val < root->valOne) {
+    if (root->middle) {
+      
+    } else {
+      
     }
   }
+}
+
+struct node * newNode(int val)
+{
+  struct node * node = malloc(sizeof(*node));
+  node->isLeaf = 0;
+  node->valOne = val;
+  node->valTwo = 0;
+  node->left = NULL;
+  node->middle = NULL;
+  node->right = NULL;
+  return node;
 }
 
 struct files * setInputAndOutput(int argc, char *args[])
